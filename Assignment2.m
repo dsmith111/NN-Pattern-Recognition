ba8596de602d5ad49a2fb827e6aa6366273e19c5
David Smith <68763658+dsmith111@users.noreply.github.com>
2020-09-25 22:01:43 -0400

    Update Assignment2.m

--- Assignment2.m
+++ Assignment2.m
@@ -1,11 +1,15 @@
+% Clear workspace and figures
 clear
 hold off;
 clf
 hold on;
+
+% Define region size and limits
 space_size = 10;
 extrema_l = -floor(space_size/2);
 extrema_h = floor(space_size/2);
-% Initiate Variables
+
+% Initiate Variables and region matrix
 w = [1 -1;2 -1;1 1];
 b = [-1;-1;3];
 wo = [1, 1, 1];
@@ -13,19 +17,24 @@ bo = -2.5;
 
 x = zeros([space_size space_size]);
 
-% Determine if nodes should be on or off
-
 % Plot decision lines
 plt_weights(w, b, extrema_l, extrema_h)
 
+% Pass nodes through the LTG to determine state
 x = activate_nodes(w, b, x, wo, bo, extrema_l);
+
 % Plot nodes
 plt_space(x, extrema_l, extrema_h)
+
+% Label Axes then draw
+ylabel('X2')
+xlabel('X1')
+title(sprintf('Single-Layer LTG NN\n'))
 
 drawnow
 
 function x = activate_nodes(w, b, x, wo, bo, extrema_l)
-    % Which node should be active based on LTGs
+    % Which node should be active based on LTG
     for i = 1:numel(x)
         [x2, x1] = ind2sub(length(x), i);
         
@@ -46,7 +55,7 @@ function plt_weights(w, b, extrema_l, extrema_h)
     px1 = extrema_l;
     px2 = extrema_h;
     
-    
+    % Plot the decision lines using w1x1 + w2x2 +w3x3 = 0 
     for i = 1:(length(w))
         m = -1*(w(i,1)/w(i,2));
         yint = -1*(b(i,1)/w(i,2));
@@ -57,6 +66,7 @@ function plt_weights(w, b, extrema_l, extrema_h)
 end
 
 function plt_space(x, extrema_l, extrema_h)
+    % Plot the nodes and visually differentiate based on state
     for i = 1:numel(x)
         [r, c] = ind2sub(length(x),i);
         if x(i) > 0
